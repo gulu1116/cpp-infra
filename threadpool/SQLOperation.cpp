dfd568dbf49ee650cdfddef56db4eeffb1852b49
GuLu <xiulinghuang136@gmail.com>
2025-08-25 10:46:41 -0700

    restore threadpool

--- threadpool/SQLOperation.cpp
+++
@@ -1,8 +0,0 @@
-
-#include "SQLOperation.h"
-#include "MySQLConn.h"
-
-void SQLOperation::Execute(MySQLConn *conn) {
-    auto result = conn->Query(sql_);
-    promise_.set_value(std::unique_ptr<sql::ResultSet>(result));
-}
